5044a6935f4927e0a272db2c0a6bdb90f7e55b90
jatinhinduja <36626637+jatinhinduja@users.noreply.github.com>
2020-09-11 11:17:09 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,41 @@
 #!/usr/bin/env groovy
 
+
+import groovy.yaml.YamlSlurper
+ 
+
 def hello(){
     echo "Hello, world from hello()"
+    def configYaml = '''\
+---
+application: "Sample App"
+users:
+- name: "mrhaki"
+  likes:
+  - Groovy
+  - Clojure
+  - Java
+- name: "Hubert"
+  likes:
+  - Apples
+  - Bananas
+connections:
+- "WS1"
+- "WS2"
+'''
+ 
+// Parse the YAML.
+def config = new YamlSlurper().parseText(configYaml)
+ 
+assert config.application == 'Sample App'
+ 
+assert config.users.size() == 2
+assert config.users[0] == [name: 'mrhaki', likes: ['Groovy', 'Clojure', 'Java']]
+assert config.users[1] == [name: 'Hubert', likes: ['Apples', 'Bananas']]
+ 
+assert config.connections == ['WS1', 'WS2']
+
+
 }
 
 def call(String environment = 'dev') {

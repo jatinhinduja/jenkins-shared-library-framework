44443649248fb77ff93c1a426fe2efd6bb6a8abe
jatinhinduja <36626637+jatinhinduja@users.noreply.github.com>
2020-09-11 12:29:59 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -7,18 +7,19 @@
 def hello(){
     //data = readYaml file: "pipeline-library-demo/vars/test.yml"
     def datas = readYaml text: """
-  dev:
-    account_id: '12312142'
-    apiKey: JENK-YY92WIWSI4W6RJMZVMRAO6G2RFR
-  qa:
-    account_id: '12312144'
-    apiKey: JENK-21L7AP9HTFJEGAQO78D8Q9WTNF5
-  staging:
-    account_id: '12312143'
-    apiKey: JENK-OGJVRHIZKKUYWL4ESYLLE2IEYIR
-  prod:
-    account_id: '12312145'
-    apiKey: JENK-AC966NDDCU3PALCJ3R2MRDFWICI
+  accounts:
+    dev:
+      account_id: '12312142'
+      apiKey: JENK-YY92WIWSI4W6RJMZVMRAO6G2RFR
+    qa:
+      account_id: '12312144'
+      apiKey: JENK-21L7AP9HTFJEGAQO78D8Q9WTNF5
+    staging:
+      account_id: '12312143'
+      apiKey: JENK-OGJVRHIZKKUYWL4ESYLLE2IEYIR
+    prod:
+      account_id: '12312145'
+      apiKey: JENK-AC966NDDCU3PALCJ3R2MRDFWICI
 
 """
        // assert datas.size() == 2

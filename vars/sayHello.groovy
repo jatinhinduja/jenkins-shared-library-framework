d445f99122910fdf19cc648ee324ac510471fc84
jatinhinduja <36626637+jatinhinduja@users.noreply.github.com>
2020-09-11 13:37:58 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -27,7 +27,7 @@ def hello(String environment){
        // assert datas[1].something == 'my second document'
 //modify
  echo "${environment}"
- echo "${datas.accounts.{environment}}"
+ echo "${datas.accounts.qa}}"
 
 
 }

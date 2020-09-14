dcf0be0624b0aba7192c1e7855840ffd718fde0d
jatinhinduja <36626637+jatinhinduja@users.noreply.github.com>
2020-09-14 13:48:24 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,7 +4,7 @@
 
  
 
-def hello(environment){
+def parse_yml(environment){
     //data = readYaml file: "pipeline-library-demo/vars/test.yml"
     def datas = readYaml text: """
   accounts:
@@ -42,6 +42,6 @@ def hello(environment){
 
 def call(String environment = 'dev') {
   echo "Hello, ${environment}."
-  hello(environment)
+  parse_yml(environment)
 }
 

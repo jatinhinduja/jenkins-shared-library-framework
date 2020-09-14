757cdf17b2f00a6114470f1484ba5b14476dcafe
jatinhinduja <36626637+jatinhinduja@users.noreply.github.com>
2020-09-14 16:38:07 +0530

    Update foo.groovy

--- src/com/org/foo.groovy
+++ src/com/org/foo.groovy
@@ -1,11 +1,9 @@
 #!/usr/bin/env groovy
-package com.cleverbuilder
+package com.org
 
-class GlobalVars {
-   static String foo = "bar"
-
-   // refer to this in a pipeline using:
-   //
-   // import com.cleverbuilder.GlobalVars
-   // println GlobalVars.foo
+class Test implements Serializable{
+  String val
+  Test(val) {
+    this.val = val
+  }
 }
